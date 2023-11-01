56a9e5cb9b5c5610a1b19b63ca78052249260213
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-31 18:51:58 -0600

    GET users in database

--- w01-project-contacts-part-1/app.js
+++ w01-project-contacts-part-1/app.js
@@ -1,21 +1,51 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const usersRouter = require("./routes/users");
-const {connectToDatabase} = require("./db/connect");
+const {ObjectId} = require("mongodb");
+const {connectToDb, getDb} = require("./db/database");
 
-connectToDatabase()
-	.then(() => {
-		console.log("Connect to database");
-
-		// Use 'users' route
-		app.use("/users", usersRouter);
-
-		// Start the express server
+// database connection
+let database;
+connectToDb((error) => {
+	if (!error) {
 		app.listen(port, () => {
+			console.log("Database Connected :)");
 			console.log(`App listening on http://localhost:${port}/users`);
 		});
-	})
-	.catch((error) => {
-		console.error(`Failed to connect to database: ${error}`);
-	});
+		database = getDb();
+	}
+});
+
+
+// Routes
+app.get("/users", (req, res) => {
+	let users = [];
+
+	database.collection("users")
+		.find()
+		.sort({firstName: 1})
+		.forEach(user => {
+			users.push(user);
+		})
+		.then(() => {
+			res.status(200).json(users);
+		})
+		.catch(() => {
+			res.status(500).json({error: "Could not fetch the users"});
+		});
+});
+
+app.get("/users/:id", (req, res) => {
+	if (ObjectId.isValid(req.params.id)) {
+		database.collection("users")
+			.findOne({_id: new ObjectId(req.params.id)})
+			.then(document => {
+				res.status(200).json(document);
+			})
+			.catch(err => {
+				res.status(500).json({error: `Could not fetch documents: ${err}`});
+			});
+	} else {
+		res.status(500).json({error: "Not a valid document id"});
+	}
+});
