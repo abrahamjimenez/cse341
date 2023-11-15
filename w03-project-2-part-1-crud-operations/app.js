e74187f3be72fc05146081390d92e31539effcdf
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-15 16:06:14 -0700

    Routes working

--- w03-project-2-part-1-crud-operations/app.js
+++ w03-project-2-part-1-crud-operations/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {ObjectId} = require("mongodb");
 
 const {connectToDb, getDb} = require("./db/database");
+const dogRoutes = require("./routes/dogRoutes");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -21,101 +22,4 @@ connectToDb((error) => {
 });
 
 
-// Routes
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
-
-app.get("/dogs", (req, res) => {
-	let dogs = [];
-
-	database.collection("dogs")
-		.find()
-		.sort({firstName: 1})
-		.forEach(user => {
-			dogs.push(user);
-		})
-		.then(() => {
-			res.status(200).json(dogs);
-		})
-		.catch(() => {
-			res.status(500).json({error: "Could not fetch the users"});
-		});
-});
-
-app.get("/dogs/:id", (req, res) => {
-	if (ObjectId.isValid(req.params.id)) {
-		database.collection("dogs")
-			.findOne({_id: new ObjectId(req.params.id)})
-			.then(document => {
-				res.status(200).json(document);
-			})
-			.catch(err => {
-				res.status(500).json({error: `Could not fetch documents: ${err}`});
-			});
-	} else {
-		res.status(500).json({error: "Not a valid document id"});
-	}
-});
-
-app.post("/dogs", (req, res) => {
-	const dog = req.body;
-
-	database.collection("dogs")
-		.insertOne(dog)
-		.then(result => {
-			res.status(201).json(result);
-		})
-		.catch(error => {
-			res.status(500).json({error: `Could not create new document: ${error}`});
-		});
-});
-
-app.delete("/dog/:id", (req, res) => {
-	if (ObjectId.isValid(req.params.id)) {
-		database.collection("users")
-			.deleteOne({_id: new ObjectId(req.params.id)})
-			.then(result => {
-				res.status(200).json(result);
-			})
-			.catch(err => {
-				res.status(500).json({error: `Could not delete the document: ${err}`});
-			});
-	} else {
-		res.status(500).json({error: "Not a valid document id"});
-	}
-});
-
-app.put("/dogs/:id", (req, res) => {
-	const updates = req.body;
-
-	if (ObjectId.isValid(req.params.id)) {
-		database.collection("users")
-			.updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
-			.then(result => {
-				res.status(200).json(result);
-			})
-			.catch(err => {
-				res.status(500).json({error: `Could not update the document: ${err}`});
-			});
-	} else {
-		res.status(500).json({error: "Not a valid document id"});
-	}
-});
-
-app.patch("/dogs/:id", (req, res) => {
-	const updates = req.body;
-
-	if (ObjectId.isValid(req.params.id)) {
-		database.collection("users")
-			.updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
-			.then(result => {
-				res.status(200).json(result);
-			})
-			.catch(err => {
-				res.status(500).json({error: `Could not update the document: ${err}`});
-			});
-	} else {
-		res.status(500).json({error: "Not a valid document id"});
-	}
-});
+app.use("/" , dogRoutes);
---
+++ w03-project-2-part-1-crud-operations/routes/dogRoutes.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const dogsController = require("../controllers/dogsController");
+const {ObjectId} = require("mongodb");
+const {getDb, connectToDb} = require("../db/database");
+
+const router = express.Router();
+
+let database;
+connectToDb((error) => {
+  if (!error) {
+    database = getDb();
+  }
+});
+
+// Routes
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+router.get("/dogs", (req, res) => {
+  let dogs = [];
+
+  database.collection("dogs")
+    .find()
+    .sort({firstName: 1})
+    .forEach(user => {
+      dogs.push(user);
+    })
+    .then(() => {
+      res.status(200).json(dogs);
+    })
+    .catch(() => {
+      res.status(500).json({error: "Could not fetch the users"});
+    });
+});
+
+router.get("/dogs/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    database.collection("dogs")
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(document => {
+        res.status(200).json(document);
+      })
+      .catch(err => {
+        res.status(500).json({error: `Could not fetch documents: ${err}`});
+      });
+  } else {
+    res.status(500).json({error: "Not a valid document id"});
+  }
+});
+
+router.post("/dogs", (req, res) => {
+  const dog = req.body;
+
+  database.collection("dogs")
+    .insertOne(dog)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({error: `Could not create new document: ${error}`});
+    });
+});
+
+router.delete("/dog/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    database.collection("users")
+      .deleteOne({_id: new ObjectId(req.params.id)})
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({error: `Could not delete the document: ${err}`});
+      });
+  } else {
+    res.status(500).json({error: "Not a valid document id"});
+  }
+});
+
+router.put("/dogs/:id", (req, res) => {
+  const updates = req.body;
+
+  if (ObjectId.isValid(req.params.id)) {
+    database.collection("users")
+      .updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({error: `Could not update the document: ${err}`});
+      });
+  } else {
+    res.status(500).json({error: "Not a valid document id"});
+  }
+});
+
+router.patch("/dogs/:id", (req, res) => {
+  const updates = req.body;
+
+  if (ObjectId.isValid(req.params.id)) {
+    database.collection("users")
+      .updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({error: `Could not update the document: ${err}`});
+      });
+  } else {
+    res.status(500).json({error: "Not a valid document id"});
+  }
+});
+
+module.exports = router;
