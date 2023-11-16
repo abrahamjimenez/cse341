b3529ed3dad3afce7ce1786a71802fc7d631829d
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-15 21:52:49 -0700

    add validation rules

--- w03-project-2-part-1-crud-operations/routes/dogRoutes.js
+++ w03-project-2-part-1-crud-operations/routes/dogRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dogsController = require("../controllers/dogsController");
+const {dogRules, validate} = require("../utilities/dogValidation")
 
 const router = express.Router();
 
 // Routes
 router.get("/", dogsController.getAllDogs);
 router.get("/:id", dogsController.getDogById);
-router.post("/", dogsController.createDog);
-router.put("/:id", dogsController.updateDog);
-router.patch("/:id", dogsController.patchDog);
+router.post("/", dogRules(), validate, dogsController.createDog);
+router.put("/:id", dogRules(), validate, dogsController.updateDog);
+router.patch("/:id", dogRules(), validate, dogsController.patchDog);
 router.delete("/:id", dogsController.deleteDog);
 
 module.exports = router;
