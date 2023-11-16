f5042b9264fbd905ebda54179b4907fd6fbf0b1c
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-15 21:00:32 -0700

    try catch code

--- w03-project-2-part-1-crud-operations/controllers/dogsController.js
+++ w03-project-2-part-1-crud-operations/controllers/dogsController.js
@@ -3,96 +3,79 @@ const {ObjectId} = require("mongodb");
 let database;
 
 exports.getAllDogs = async (req, res) => {
-  let dogs = [];
-  database = req.database;
+  try {
+    database = req.database;
 
-  database.collection("dogs")
-    .find()
-    .sort({firstName: 1})
-    .forEach(user => {
-      dogs.push(user);
-    })
-    .then(() => {
-      res.status(200).json(dogs);
-    })
-    .catch(() => {
-      res.status(500).json({error: "Could not fetch the users"});
-    });
+    const dogs = await database.collection("dogs")
+      .find()
+      .sort({firstName: 1})
+      .toArray();
+    res.status(200).json(dogs);
+  } catch (error) {
+    res.status(500).json({error: "Could not fetch the users"});
+  }
 };
 
 exports.getDogById = async (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    database.collection("dogs")
-      .findOne({_id: new ObjectId(req.params.id)})
-      .then(document => {
-        res.status(200).json(document);
-      })
-      .catch(err => {
-        res.status(500).json({error: `Could not fetch documents: ${err}`});
-      });
-  } else {
-    res.status(500).json({error: "Not a valid document id"});
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const document = await database.collection("dogs").findOne({_id: new ObjectId(req.params.id)});
+      res.status(200).json(document);
+    } else {
+      res.status(500).json({error: "Not a valid document id"});
+    }
+  } catch (err) {
+    res.status(500).json({error: `Could not fetch documents: ${err}`});
   }
 };
 
 exports.createDog = async (req, res) => {
-  const dog = req.body;
-
-  database.collection("dogs")
-    .insertOne(dog)
-    .then(result => {
-      res.status(201).json(result);
-    })
-    .catch(error => {
-      res.status(500).json({error: `Could not create new document: ${error}`});
-    });
+  try {
+    const dog = req.body;
+    const result = await database.collection("dogs").insertOne(dog);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({error: `Could not create new document: ${error}`});
+  }
 };
 
 exports.updateDog = async (req, res) => {
-  const updates = req.body;
-
-  if (ObjectId.isValid(req.params.id)) {
-    database.collection("dogs")
-      .updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
-      .then(result => {
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        res.status(500).json({error: `Could not update the document: ${err}`});
-      });
-  } else {
-    res.status(500).json({error: "Not a valid document id"});
+  try {
+    const updates = req.body;
+    if (ObjectId.isValid(req.params.id)) {
+      const result = await database.collection("dogs").updateOne({_id: new ObjectId(req.params.id)}, {$set: updates});
+      res.status(200).json(result);
+    } else {
+      res.status(500).json({error: "Not a valid document id"});
+    }
+  } catch (err) {
+    res.status(500).json({error: `Could not update the document: ${err}`});
   }
 };
 
 exports.patchDog = async (req, res) => {
-  const updates = req.body;
-
-  if (ObjectId.isValid(req.params.id)) {
-    database.collection("dogs")
-      .updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
-      .then(result => {
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        res.status(500).json({error: `Could not update the document: ${err}`});
-      });
-  } else {
-    res.status(500).json({error: "Not a valid document id"});
+  try {
+    const updates = req.body;
+    if (ObjectId.isValid(req.params.id)) {
+      const result = await database.collection("dogs").updateOne({_id: new ObjectId(req.params.id)}, {$set: updates});
+      res.status(200).json(result);
+    } else {
+      res.status(500).json({error: "Not a valid document id"});
+    }
+  } catch (err) {
+    res.status(500).json({error: `Could not update the document: ${err}`});
   }
 };
 
 exports.deleteDog = async (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    database.collection("dogs")
-      .deleteOne({_id: new ObjectId(req.params.id)})
-      .then(result => {
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        res.status(500).json({error: `Could not delete the document: ${err}`});
-      });
-  } else {
-    res.status(500).json({error: "Not a valid document id"});
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const result = await database.collection("dogs").deleteOne({_id: new ObjectId(req.params.id)});
+      res.status(200).json(result);
+    } else {
+      res.status(500).json({error: "Not a valid document id"});
+    }
+  } catch (err) {
+    res.status(500).json({error: `Could not delete the document: ${err}`});
   }
 };
