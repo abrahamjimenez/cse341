57a0c88b6630e226b9761b33ecb69fbb31521942
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-22 16:55:51 -0700

    Swagger: Updated config

--- w04-project-2-part-2-crud-operations-2/utils/swagger.js
+++ w04-project-2-part-2-crud-operations-2/utils/swagger.js
@@ -2,14 +2,15 @@ import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    title: 'Animals API',
+    description: 'Perform CRUD operations with animals!'
   },
-  host: 'localhost:5500'
+  host: 'localhost:5500',
+  basePath: '/dashboard'
 };
 
 const outputFile = './swagger-output.json';
-const routes = ["../routes/userRoutes.js"];
+const routes = ["../routes/dashboardRoutes.js"];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
