75ad2868d07292e8df92bed7d5eb73df942da8d1
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-22 11:29:30 -0700

    Passport: Successful Login and Redirect

--- w04-project-2-part-2-crud-operations-2/config/passport.js
+++ w04-project-2-part-2-crud-operations-2/config/passport.js
@@ -8,7 +8,26 @@ export default function (passport) {
       callbackURL: "/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log(profile)
+      const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        image: profile.photos[0].value
+      }
+
+      try {
+        let user = await User.findOne({googleId: profile.Id})
+
+        if (user) {
+          done(null, user)
+        } else {
+          user = await User.create(newUser)
+          done(null, user)
+        }
+      } catch (err) {
+        console.error(err)
+      }
     }));
 
   passport.serializeUser((user, done) => {
@@ -16,6 +35,11 @@ export default function (passport) {
   })
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
+    try {
+      const user = User.findById(id)
+      done(null, user)
+    } catch (err) {
+      done(err, null)
+    }
   })
 }
