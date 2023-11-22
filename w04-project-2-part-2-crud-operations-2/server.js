793b9db3ad697ecdb6ddb56f4222914cd2a277f2
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-22 09:56:09 -0700

    Database: MongoDB connection

--- w04-project-2-part-2-crud-operations-2/server.js
+++ w04-project-2-part-2-crud-operations-2/server.js
@@ -2,11 +2,15 @@ import express from "express"
 import dotenv from "dotenv"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocument from "./utils/swagger-output.json" assert {type: "json"};
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 5500
+
+// Connect to MongoDB
+connectDB()
 
 // Swagger Middleware
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
