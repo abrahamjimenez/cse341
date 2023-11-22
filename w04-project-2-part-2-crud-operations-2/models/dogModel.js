5fb6b475e19d6e624bb96b9ed57471f555be5791
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-22 14:55:08 -0700

    Models: dog and horse models

---
+++ w04-project-2-part-2-crud-operations-2/models/dogModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const dogSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  breed: String,
+  size: String,
+  temperament: [String],
+  life_span: String,
+  origin: String,
+  color: [String],
+  characteristics: {
+    intelligence: String,
+    shedding_level: String,
+    exercise_needs: String
+  },
+  token: String
+});
+
+const Dog = mongoose.model('Dog', dogSchema);
+
+export default Dog;
---
+++ w04-project-2-part-2-crud-operations-2/models/horseModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const horseSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  breed: String,
+  size: String,
+  temperament: [String],
+  life_span: String,
+  origin: String,
+  color: [String],
+  characteristics: {
+    intelligence: String,
+    shedding_level: String,
+    exercise_needs: String
+  }
+});
+
+const Horse = mongoose.model('Horse', horseSchema);
+
+export default Horse;
