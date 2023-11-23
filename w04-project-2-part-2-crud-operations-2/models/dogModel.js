6c8dcb293f899b30c4be480ac922e0ce31e94e83
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-11-22 21:42:33 -0700

    Model: Updated Models

--- w04-project-2-part-2-crud-operations-2/models/dogModel.js
+++ w04-project-2-part-2-crud-operations-2/models/dogModel.js
@@ -4,15 +4,13 @@ const Schema = mongoose.Schema;
 const dogSchema = new Schema({
   breed: String,
   size: String,
-  temperament: [String],
+  temperament: String,
   life_span: String,
   origin: String,
-  color: [String],
-  characteristics: {
-    intelligence: String,
-    shedding_level: String,
-    exercise_needs: String
-  }
+  color: String,
+  intelligence: String,
+  shedding_level: String,
+  exercise_needs: String
 });
 
 const Dog = mongoose.model('Dog', dogSchema);
--- w04-project-2-part-2-crud-operations-2/models/horseModel.js
+++ w04-project-2-part-2-crud-operations-2/models/horseModel.js
@@ -4,15 +4,13 @@ const Schema = mongoose.Schema;
 const horseSchema = new Schema({
   breed: String,
   size: String,
-  temperament: [String],
+  temperament: String,
   life_span: String,
   origin: String,
-  color: [String],
-  characteristics: {
-    intelligence: String,
-    shedding_level: String,
-    exercise_needs: String
-  }
+  color: String,
+  intelligence: String,
+  shedding_level: String,
+  exercise_needs: String
 });
 
 const Horse = mongoose.model('Horse', horseSchema);
